ba9484ba7ba63831d09fc10869460c5306f0ed96
Dalita <dalita.avanessian@gmail.com>
2016-09-17 20:45:22 +0400

    adding description

--- stars.js
+++ stars.js
@@ -1,13 +1,29 @@
+/*
+    *       = floor 4, line = 1
+   ***      = floor 3, line = 2
+  *****     = floor 2, line = 3
+ *******    = floor 1, line = 4
+*********   = floor 0, line = 5
+
+Q:  How to identify the number of stars and spaces?
+
+A:  If n = the line number from top, then n X 2 - 1 = number of required stars.
+    e.g: If line = 4, then 4 X 2 - 1 = 7 stars required.
+
+    Starting from floor = 0, adding one space to the top.
+    e.g: If floor = 3, then number of spaces = 3.
+*/
+
 const print = function(character, num) {
   if(num == 0) return '';
   return character + print(character, num - 1);
 }
 
-const triangle = function(height, currentLine) {
-  if(currentLine == undefined) currentLine = 0;
-  if(height == 0) return;
+const triangle = function(line, currentFloor) {
+  if(currentFloor == undefined) currentFloor = 0;
+  if(line == 0) return;
 
-  return triangle(height - 1, currentLine + 1) + console.log(print(' ', currentLine) + print('*', 2 * height - 1));
+  return triangle(line - 1, currentFloor + 1) + console.log(print(' ', currentFloor) + print('*', 2 * line - 1));
 }
 
 triangle(5);
